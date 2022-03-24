32f91736e98ae7cf9b77bc5e66e78ea0459c13ac
dzq <530858106@qq.com>
2022-03-24 17:14:12 +0800

    方法二：Html + ClickableSpan

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # TextView中识别网页链接，并可做点击链接操作
 
-　实现思路：SpannableString 设置 ClickableSpan
+　实现思路：
+
+# 方法一：正则匹配 + ClickableSpan ，SpannableString 设置 ClickableSpan
 
     1、识别网页链接，用正则表达式去匹配。
     2、继承ClickableSpan，重写相关方法
+
+# 方法二：Html + ClickableSpan
--- app/src/main/java/com/dzq/identifylink/MainActivity.java
+++ app/src/main/java/com/dzq/identifylink/MainActivity.java
@@ -3,19 +3,23 @@ package com.dzq.identifylink;
 import android.content.Context;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.Spannable;
 import android.text.SpannableString;
+import android.text.SpannableStringBuilder;
 import android.text.Spanned;
 import android.text.TextPaint;
 import android.text.method.LinkMovementMethod;
 import android.text.style.ClickableSpan;
+import android.text.style.URLSpan;
 import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.material.snackbar.Snackbar;
 import com.dzq.identifylink.databinding.ActivityMainBinding;
+import com.google.android.material.snackbar.Snackbar;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -39,6 +43,8 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        // 方法一：正则匹配 + ClickableSpan
         String content = "这是一段包含网址的文字，点击可以进行相关操作：第一个网址 https://www.baidu.com/，第二个网址 https://www.jd.com/";
         SpannableString sps = new SpannableString(content);
         Pattern pattern = Pattern.compile("[http|https]+[://]+[0-9A-Za-z:/[-]_#[?][=][.]]*", Pattern.CASE_INSENSITIVE);
@@ -53,6 +59,24 @@ public class MainActivity extends AppCompatActivity {
         }
         binding.tvContent.setText(sps);
         binding.tvContent.setMovementMethod(LinkMovementMethod.getInstance());
+
+        // 方法二：Html + ClickableSpan
+        content = "这是一段包含网址的文字，点击可以进行相关操作：第一个网址 <a href='http://www.baidu.com'>https://www.baidu.com/</a>，第二个网址 <a href='http://www.jd.com'>https://www.jd.com/</a>";
+        binding.tvContent2.setText(Html.fromHtml(content));
+        binding.tvContent2.setMovementMethod(LinkMovementMethod.getInstance());
+        CharSequence text = binding.tvContent2.getText();
+        if (text instanceof Spannable) {
+            Spannable sp = (Spannable) text;
+            URLSpan[] oldUrlSpans = sp.getSpans(0, text.length(), URLSpan.class);
+            SpannableStringBuilder spannableStringBuilder = new SpannableStringBuilder(text);
+            for (URLSpan oldUrlSpan : oldUrlSpans) {
+                spannableStringBuilder.removeSpan(oldUrlSpan);
+                ClickableSpan customURLSpan = new MyClickSpan(this, oldUrlSpan.getURL(), Color.parseColor("#00ff00"));
+                spannableStringBuilder.setSpan(customURLSpan, sp.getSpanStart(oldUrlSpan),
+                        sp.getSpanEnd(oldUrlSpan), Spannable.SPAN_EXCLUSIVE_INCLUSIVE);
+            }
+            binding.tvContent2.setText(spannableStringBuilder);
+        }
     }
 
     class MyClickSpan extends ClickableSpan {
